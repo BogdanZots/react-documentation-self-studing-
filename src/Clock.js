45bcf963bc71bbea9121c12364197a2ec6520790
Zots Bohdan <bzots@ukr.net>
2021-08-08 15:15:28 +0300

    the last update

--- src/Clock.js
+++ src/Clock.js
@@ -3,33 +3,43 @@ import React, {Component} from 'react';
 class Clock extends Component {
     constructor(props) {
         super(props);
-        this.state = {date: new Date()}
+        this.state = {
+            date: new Date()
+        }
     }
 
     changeState() {
-        this.setState({
-            date : new Date
-        })
+        this.setState({date: new Date})
+        console.log('ya net')
     }
-    update(){
-      this.TidemId = setInterval(this.changeState.bind(this),1000)
+    update() {
+        this.TidemId = setInterval(this.changeState.bind(this), 1000)
+        console.log('ya')
     }
 
-    componentDidMount(){
-        {this.update()}
+    componentDidMount() {
+        {
+            this.update()
+        }
     }
-    componentWillUnmount(){
+    componentWillUnmount() {
         clearInterval(this.TidemId)
     }
 
     render() {
         return (
             <div>
-                {this.state.date.toLocaleTimeString()}
-                <button onClick={this.componentWillUnmount.bind(this)}></button>
+                {this
+                    .state
+                    .date
+                    .toLocaleTimeString()}
+                <button
+                    onClick={this
+                    .componentWillUnmount
+                    .bind(this)}></button>
             </div>
         );
     }
-} 
+}
 
 export default Clock;
